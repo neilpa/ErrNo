bcf267d1005b2b0f0e83e8dd7899b7d3cb0e2cfb
Neil Pankey <npankey@gmail.com>
2015-03-20 23:39:23 -0700

    Minor formatting

--- ErrNo/ErrNo.swift
+++ ErrNo/ErrNo.swift
@@ -14,13 +14,10 @@ public enum ErrNo : Int32 {
     case EBADF /* Bad file descriptor */
     case ECHILD /* No child processes */
     case EDEADLK /* Resource deadlock avoided */
-    /* 11 was EAGAIN */
     case ENOMEM /* Cannot allocate memory */
     case EACCES /* Permission denied */
     case EFAULT /* Bad address */
-
     case ENOTBLK /* Block device required */
-
     case EBUSY /* Device / Resource busy */
     case EEXIST /* File exists */
     case EXDEV /* Cross-device link */
@@ -56,24 +53,9 @@ public enum ErrNo : Int32 {
     case EPROTOTYPE /* Protocol wrong type for socket */
     case ENOPROTOOPT /* Protocol not available */
     case EPROTONOSUPPORT /* Protocol not supported */
-
     case ESOCKTNOSUPPORT /* Socket type not supported */
-
     case ENOTSUP /* Operation not supported */
-
-    /*
-    * This is the same for binary and source copmpatability, unless compiling
-    * the kernel itself, or compiling __DARWIN_UNIX03; if compiling for the
-    * kernel, the correct value will be returned.  If compiling non-POSIX
-    * source, the kernel return value will be converted by a stub in libc, and
-    * if compiling source with __DARWIN_UNIX03, the conversion in libc is not
-    * done, and the caller gets the expected (discrete) value.
-    */
-    /* Operation not supported on socket */
-    /* !__DARWIN_UNIX03 && !KERNEL */
-
     case EPFNOSUPPORT /* Protocol family not supported */
-
     case EAFNOSUPPORT /* Address family not supported by protocol family */
     case EADDRINUSE /* Address already in use */
     case EADDRNOTAVAIL /* Can't assign requested address */
@@ -87,10 +69,8 @@ public enum ErrNo : Int32 {
     case ENOBUFS /* No buffer space available */
     case EISCONN /* Socket is already connected */
     case ENOTCONN /* Socket is not connected */
-
     case ESHUTDOWN /* Can't send after socket shutdown */
     case ETOOMANYREFS /* Too many references: can't splice */
-
     case ETIMEDOUT /* Operation timed out */
     case ECONNREFUSED /* Connection refused */
 
@@ -98,22 +78,17 @@ public enum ErrNo : Int32 {
     case ENAMETOOLONG /* File name too long */
 
     /* should be rearranged */
-
     case EHOSTDOWN /* Host is down */
-
     case EHOSTUNREACH /* No route to host */
     case ENOTEMPTY /* Directory not empty */
 
     /* quotas & mush */
-
     case EPROCLIM /* Too many processes */
     case EUSERS /* Too many users */
-
     case EDQUOT /* Disc quota exceeded */
 
     /* Network File System */
     case ESTALE /* Stale NFS file handle */
-
     case EREMOTE /* Too many levels of remote in path */
     case EBADRPC /* RPC struct is bad */
     case ERPCMISMATCH /* RPC version wrong */
@@ -135,7 +110,6 @@ public enum ErrNo : Int32 {
     case EOVERFLOW /* Value too large to be stored in data type */
 
     /* Program loading errors */
-
     case EBADEXEC /* Bad executable */
     case EBADARCH /* Bad CPU type in executable */
     case ESHLIBVERS /* Shared library version mismatch */
@@ -146,7 +120,6 @@ public enum ErrNo : Int32 {
     case EIDRM /* Identifier removed */
     case ENOMSG /* No message of desired type */
     case EILSEQ /* Illegal byte sequence */
-
     case ENOATTR /* Attribute not found */
 
     case EBADMSG /* Bad message */
@@ -158,9 +131,7 @@ public enum ErrNo : Int32 {
     case EPROTO /* Protocol error */
     case ETIME /* STREAM ioctl timeout */
     
-    /* This value is only discrete when compiling __DARWIN_UNIX03, or KERNEL */
     case EOPNOTSUPP /* Operation not supported on socket */
-    /* __DARWIN_UNIX03 || KERNEL */
     
     case ENOPOLICY /* No such policy registered */
     
