5b11d25d49602ddcc326368c136aab9c532c3236
Neil Pankey <npankey@gmail.com>
2015-03-20 23:07:50 -0700

    Xcode project

---
+++ ErrNo/ErrNo.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+
+/// Project version number for ErrNo.
+extern double ErrNoVersionNumber;
+
+/// Project version string for ErrNo.
+extern const unsigned char ErrNoVersionString[];
+
