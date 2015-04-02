206ec1b8a9ceb40026ee0a1d16f92b9ea2053305
Neil Pankey <npankey@gmail.com>
2015-04-01 21:03:51 -0700

    Switch to Rob's Result

--- ErrNo/Result.swift
+++ ErrNo/Result.swift
@@ -6,19 +6,19 @@
 //  Copyright (c) 2015 Neil Pankey. All rights reserved.
 //
 
-import LlamaKit
+import Result
 
 /// Wraps `lastError()` in a failed `Result`.
 public func lastFailure<T>() -> Result<T, ErrNo> {
-    return failure(lastError()!)
+    return Result.failure(lastError()!)
 }
 
 /// Checks `returnCode` and wraps it in a `Result`. Returns a `failure` if < 0, an empty `success` otherwise.
 public func try(returnCode: Int32) -> Result<(), ErrNo> {
-    return returnCode < 0 ? lastFailure() : success(())
+    return returnCode < 0 ? lastFailure() : Result.success(())
 }
 
 /// Checks `returnCode` and wraps it in a `Result`. Returns a `failure` if < 0, otherwise `success` with the value from `ifSuccessful`.
 public func tryMap<T>(returnCode: Int32, ifSuccessful: Int32 -> T) -> Result<T, ErrNo> {
-    return returnCode < 0 ? lastFailure() : success(ifSuccessful(returnCode))
+    return returnCode < 0 ? lastFailure() : Result.success(ifSuccessful(returnCode))
 }
