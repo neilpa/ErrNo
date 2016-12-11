f25cca802a7bf42a35842dea110c92a546e164ef
Neil Pankey <npankey@gmail.com>
2016-12-11 00:37:04 -0800

    Convert to Swift 3
    Seg-faults the compiler currently. However copying the file into a
    new project works just fine.

--- ErrNo/ErrNo.swift
+++ ErrNo/ErrNo.swift
@@ -10,7 +10,6 @@ public func lastError() -> ErrNo? {
 /// Wrapper around errno values from the C standard library.
 public enum ErrNo : errno_t {
     // MARK: Basic
-
     /// Operation not permitted
     case EPERM      = 1
     /// No such file or directory
@@ -77,14 +76,12 @@ public enum ErrNo : errno_t {
     case EPIPE      = 32
 
     // MARK: Math software
-
     /// Numerical argument out of domain
     case EDOM       = 33
     /// Result too large
     case ERANGE     = 34
 
     // MARK: Non-blocking and interrupt i/o
-
     /// Resource temporarily unavailable
     case EAGAIN         = 35
     /// Operation would block
@@ -95,7 +92,6 @@ public enum ErrNo : errno_t {
     case EALREADY       = 37
 
     // MARK: IPC/network software -- argument errors
-
     /// Socket operation on non-socket
     case ENOTSOCK           = 38
     /// Destination address required
@@ -122,7 +118,6 @@ public enum ErrNo : errno_t {
     case EADDRNOTAVAIL      = 49
 
     // MARK: IPC/network software -- operational errors
-
     /// Network is down
     case ENETDOWN       = 50
     /// Network is unreachable
@@ -161,7 +156,6 @@ public enum ErrNo : errno_t {
     case ENOTEMPTY      = 66
 
     // MARK: Quotas & mush
-
     /// Too many processes
     case EPROCLIM   = 67
     /// Too many users
@@ -170,7 +164,6 @@ public enum ErrNo : errno_t {
     case EDQUOT     = 69
 
     // MARK: Network File System
-
     /// Stale NFS file handle
     case ESTALE         = 70
     /// Too many levels of remote in path
@@ -199,7 +192,6 @@ public enum ErrNo : errno_t {
     case ENEEDAUTH  = 81
 
     // MARK: Intelligent device errors
-
     /// Device power is off
     case EPWROFF    = 82
     /// Device error, e.g. paper out
@@ -209,7 +201,6 @@ public enum ErrNo : errno_t {
     case EOVERFLOW  = 84
 
     // MARK: Program loading errors
-
     /// Bad executable
     case EBADEXEC   = 85
     /// Bad CPU type in executable
@@ -265,9 +256,8 @@ public enum ErrNo : errno_t {
     //case ELAST    = 106
 }
 
-// MARK: Printable
-
-extension ErrNo : Printable {
+// MARK: CustomStringConvertible
+extension ErrNo : CustomStringConvertible {
 
     /// Returns the error message and code.
     public var description: String {
@@ -276,113 +266,6 @@ extension ErrNo : Printable {
 
     /// Lookup error message specific to `ErrNo`.
     public var message: String {
-        switch self {
-        case EPERM:           return "Operation not permitted"
-        case ENOENT:          return "No such file or directory"
-        case ESRCH:           return "No such process"
-        case EINTR:           return "Interrupted system call"
-        case EIO:             return "Input/output error"
-        case ENXIO:           return "Device not configured"
-        case E2BIG:           return "Argument list too long"
-        case ENOEXEC:         return "Exec format error"
-        case EBADF:           return "Bad file descriptor"
-        case ECHILD:          return "No child processes"
-        case EDEADLK:         return "Resource deadlock avoided"
-        case ENOMEM:          return "Cannot allocate memory"
-        case EACCES:          return "Permission denied"
-        case EFAULT:          return "Bad address"
-        case ENOTBLK:         return "Block device required"
-        case EBUSY:           return "Device / Resource busy"
-        case EEXIST:          return "File exists"
-        case EXDEV:           return "Cross-device link"
-        case ENODEV:          return "Operation not supported by device"
-        case ENOTDIR:         return "Not a directory"
-        case EISDIR:          return "Is a directory"
-        case EINVAL:          return "Invalid argument"
-        case ENFILE:          return "Too many open files in system"
-        case EMFILE:          return "Too many open files"
-        case ENOTTY:          return "Inappropriate ioctl for device"
-        case ETXTBSY:         return "Text file busy"
-        case EFBIG:           return "File too large"
-        case ENOSPC:          return "No space left on device"
-        case ESPIPE:          return "Illegal seek"
-        case EROFS:           return "Read-only file system"
-        case EMLINK:          return "Too many links"
-        case EPIPE:           return "Broken pipe"
-        case EDOM:            return "Numerical argument out of domain"
-        case ERANGE:          return "Result too large"
-        case EAGAIN:          return "Resource temporarily unavailable"
-        case EINPROGRESS:     return "Operation now in progress"
-        case EALREADY:        return "Operation already in progress"
-        case ENOTSOCK:        return "Socket operation on non-socket"
-        case EDESTADDRREQ:    return "Destination address required"
-        case EMSGSIZE:        return "Message too long"
-        case EPROTOTYPE:      return "Protocol wrong type for socket"
-        case ENOPROTOOPT:     return "Protocol not available"
-        case EPROTONOSUPPORT: return "Protocol not supported"
-        case ESOCKTNOSUPPORT: return "Socket type not supported"
-        case ENOTSUP:         return "Operation not supported"
-        case EPFNOSUPPORT:    return "Protocol family not supported"
-        case EAFNOSUPPORT:    return "Address family not supported by protocol family"
-        case EADDRINUSE:      return "Address already in use"
-        case EADDRNOTAVAIL:   return "Can't assign requested address"
-        case ENETDOWN:        return "Network is down"
-        case ENETUNREACH:     return "Network is unreachable"
-        case ENETRESET:       return "Network dropped connection on reset"
-        case ECONNABORTED:    return "Software caused connection abort"
-        case ECONNRESET:      return "Connection reset by peer"
-        case ENOBUFS:         return "No buffer space available"
-        case EISCONN:         return "Socket is already connected"
-        case ENOTCONN:        return "Socket is not connected"
-        case ESHUTDOWN:       return "Can't send after socket shutdown"
-        case ETOOMANYREFS:    return "Too many references: can't splice"
-        case ETIMEDOUT:       return "Operation timed out"
-        case ECONNREFUSED:    return "Connection refused"
-        case ELOOP:           return "Too many levels of symbolic links"
-        case ENAMETOOLONG:    return "File name too long"
-        case EHOSTDOWN:       return "Host is down"
-        case EHOSTUNREACH:    return "No route to host"
-        case ENOTEMPTY:       return "Directory not empty"
-        case EPROCLIM:        return "Too many processes"
-        case EUSERS:          return "Too many users"
-        case EDQUOT:          return "Disc quota exceeded"
-        case ESTALE:          return "Stale NFS file handle"
-        case EREMOTE:         return "Too many levels of remote in path"
-        case EBADRPC:         return "RPC struct is bad"
-        case ERPCMISMATCH:    return "RPC version wrong"
-        case EPROGUNAVAIL:    return "RPC prog. not avail"
-        case EPROGMISMATCH:   return "Program version wrong"
-        case EPROCUNAVAIL:    return "Bad procedure for program"
-        case ENOLCK:          return "No locks available"
-        case ENOSYS:          return "Function not implemented"
-        case EFTYPE:          return "Inappropriate file type or format"
-        case EAUTH:           return "Authentication error"
-        case ENEEDAUTH:       return "Need authenticator"
-        case EPWROFF:         return "Device power is off"
-        case EDEVERR:         return "Device error, e.g. paper out"
-        case EOVERFLOW:       return "Value too large to be stored in data type"
-        case EBADEXEC:        return "Bad executable"
-        case EBADARCH:        return "Bad CPU type in executable"
-        case ESHLIBVERS:      return "Shared library version mismatch"
-        case EBADMACHO:       return "Malformed Macho file"
-        case ECANCELED:       return "Operation canceled"
-        case EIDRM:           return "Identifier removed"
-        case ENOMSG:          return "No message of desired type"
-        case EILSEQ:          return "Illegal byte sequence"
-        case ENOATTR:         return "Attribute not found"
-        case EBADMSG:         return "Bad message"
-        case EMULTIHOP:       return "Reserved"
-        case ENODATA:         return "No message available on STREAM"
-        case ENOLINK:         return "Reserved"
-        case ENOSR:           return "No STREAM resources"
-        case ENOSTR:          return "Not a STREAM"
-        case EPROTO:          return "Protocol error"
-        case ETIME:           return "STREAM ioctl timeout"
-        case EOPNOTSUPP:      return "Operation not supported on socket"
-        case ENOPOLICY:       return "No such policy registered"
-        case ENOTRECOVERABLE: return "State not recoverable"
-        case EOWNERDEAD:      return "Previous owner died"
-        case EQFULL:          return "Interface output queue is full"
-        }
+        return String(cString: strerror(self.rawValue))
     }
 }
